e4c487374b64114bfcb1bc84751ab767d71af86a
taesubhan <tshan499@gmail.com>
2026-01-02 00:37:26 -0800

    Add size limit to photo image uploader
    Added code that limits the size of the photo being sent to the multer
    endpoint.

--- server/dist/routes/receiptRouter.js
+++ server/dist/routes/receiptRouter.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import multer from 'multer';
 import path from 'path';
+const sizeLimitMB = 5; //in Megabytes
 const receiptRouter = Router();
 const storage = multer.diskStorage({
+    // Defines the location and filename of the photo that the user uploads
     destination: (req, file, cb) => {
         cb(null, './dist/images/receipts');
     },
@@ -11,7 +13,12 @@ const storage = multer.diskStorage({
         cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
     }
 });
-const upload = multer({ storage: storage });
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * sizeLimitMB
+    }
+});
 receiptRouter.post('/upload', upload.single('avatar'), (req, res) => {
     // name in upload.single('name') needs to match with front-end name attribute
     console.log('upload triggered!');
--- server/src/routes/receiptRouter.ts
+++ server/src/routes/receiptRouter.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import multer from 'multer';
 import path from 'path';
 
+const sizeLimitMB: number = 5 //in Megabytes
 const receiptRouter = Router();
 const storage = multer.diskStorage({
     // Defines the location and filename of the photo that the user uploads
@@ -14,7 +15,12 @@ const storage = multer.diskStorage({
     }
 })
 
-const upload = multer({storage: storage});
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * sizeLimitMB
+    }
+});
 
 receiptRouter.post('/upload', upload.single('avatar'), (req, res) => { 
     // name in upload.single('name') needs to match with front-end name attribute
@@ -23,6 +29,18 @@ receiptRouter.post('/upload', upload.single('avatar'), (req, res) => {
     res.send(req.file);
     console.log('image received');
 })
+
+// Consider moving to error module later
+// receiptRouter.use((err, req, res, next) => {
+//     if (err instanceof multer.MulterError) {
+//         if (err.code === 'LIMIT_FILE_SIZE') {
+//             return res.status(400).send(`File size is too large. Max size is ${sizeLimitMB}MB`);
+//         }
+
+//         return res.status(400).send(`Multer error: ${err.message}`);
+//     }
+//     next(err);
+// })
 
 
 export default receiptRouter;
